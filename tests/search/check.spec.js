894013f47c6158be6393d0234ef1d602adbb5728
TadaKakehashi <aryan333498@gmail.com>
2026-05-23 16:28:02 +0530

    Search Option check file

--- tests/search/check.spec.js
+++ tests/search/check.spec.js
@@ -28,15 +28,22 @@ test.describe("Search Module", () => {
     await page.getByRole("textbox", { name: "Search razer.com" }).fill("Blade");
   });
 
-  //Search textbox input result visible 
-  test('test', async ({ page }) => {
-  await page.goto('https://www.razer.com/');
-  await page.getByRole('button', { name: 'Accept All' }).click();
-  await page.getByLabel('global', { exact: true }).locator('cx-page-slot').filter({ hasText: 'Search razer.comBegin typing' }).click();
-  await page.getByRole('textbox', { name: 'Search razer.com' }).click();
-  await page.getByRole('textbox', { name: 'Search razer.com' }).fill('BlackWidow');
-  await page.getByRole('textbox', { name: 'Search razer.com' }).press('Enter');
-  await page.getByRole('textbox', { name: 'Search razer.com' }).click();
-});
-
+  //Search textbox input result visible
+  test("test", async ({ page }) => {
+    await page.goto("https://www.razer.com/");
+    await page.getByRole("button", { name: "Accept All" }).click();
+    await page
+      .getByLabel("global", { exact: true })
+      .locator("cx-page-slot")
+      .filter({ hasText: "Search razer.comBegin typing" })
+      .click();
+    await page.getByRole("textbox", { name: "Search razer.com" }).click();
+    await page
+      .getByRole("textbox", { name: "Search razer.com" })
+      .fill("BlackWidow");
+    await page
+      .getByRole("textbox", { name: "Search razer.com" })
+      .press("Enter");
+    await page.getByRole("textbox", { name: "Search razer.com" }).click();
+  });
 });
